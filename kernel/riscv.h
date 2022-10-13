d31d0aaa9cab9c661b513d9d0376fd9d5f9e661f
zhouzihong <aqua5201314@outlook.com>
2022-10-13 22:50:19 +0800

    ADD: notes

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -360,4 +360,3 @@ r_fp()
   uint64 x;
   asm volatile("mv %0, s0" : "=r" (x));
   return x;
-}
--- kernel/trap.c
+++ kernel/trap.c
@@ -1,10 +1,10 @@
-#include "types.h"
-#include "param.h"
+#include "defs.h"
 #include "memlayout.h"
+#include "param.h"
+#include "proc.h"
 #include "riscv.h"
 #include "spinlock.h"
-#include "proc.h"
-#include "defs.h"
+#include "types.h"
 
 struct spinlock tickslock;
 uint ticks;
@@ -16,45 +16,44 @@ void kernelvec();
 
 extern int devintr();
 
-void
-trapinit(void)
-{
-  initlock(&tickslock, "time");
-}
+void trapinit(void) { initlock(&tickslock, "time"); }
 
 // set up to take exceptions and traps while in the kernel.
-void
-trapinithart(void)
-{
-  w_stvec((uint64)kernelvec);
-}
+void trapinithart(void) { w_stvec((uint64)kernelvec); }
 
 //
 // handle an interrupt, exception, or system call from user space.
 // called from trampoline.S
 //
-void
-usertrap(void)
-{
+void usertrap(void) {
+  // 1. 如果是时钟中断
+  //   a. 如果进程有alarm中断
+  //   b. 进程alarm中断剩余时间--
+  //   c. 如果alarm中断剩余时间小于等于0
+  //   d. 要求alarm中断标记已结束
+  //   e. 开启alarm中断标记未结束
+  //   f. 进程alarm中断上下文保存进程中断上下文
+  //   g. 进程中断上下文的通用寄存器存放进程alarm中断函数指针
+  //   h. 使用进程管理器的系统调用yield
+  //   i. 返回后，使用陷阱管理器的从内核态转为用户态。
+
   int which_dev = 0;
 
-  if((r_sstatus() & SSTATUS_SPP) != 0)
-    panic("usertrap: not from user mode");
+  if ((r_sstatus() & SSTATUS_SPP) != 0) panic("usertrap: not from user mode");
 
   // send interrupts and exceptions to kerneltrap(),
   // since we're now in the kernel.
   w_stvec((uint64)kernelvec);
 
   struct proc *p = myproc();
-  
+
   // save user program counter.
   p->trapframe->epc = r_sepc();
-  
-  if(r_scause() == 8){
+
+  if (r_scause() == 8) {
     // system call
 
-    if(p->killed)
-      exit(-1);
+    if (p->killed) exit(-1);
 
     // sepc points to the ecall instruction,
     // but we want to return to the next instruction.
@@ -65,7 +64,7 @@ usertrap(void)
     intr_on();
 
     syscall();
-  } else if((which_dev = devintr()) != 0){
+  } else if ((which_dev = devintr()) != 0) {
     // ok
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
@@ -73,17 +72,17 @@ usertrap(void)
     p->killed = 1;
   }
 
-  if(p->killed)
-    exit(-1);
+  if (p->killed) exit(-1);
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2) {
-    if(p->alarm_interval != 0) {
-      if(--p->alarm_ticks <= 0) {
-        if(!p->alarm_goingoff) { // make sure alarm_handler isn't already running
+  if (which_dev == 2) {
+    if (p->alarm_interval != 0) {
+      if (--p->alarm_ticks <= 0) {
+        if (!p->alarm_goingoff) {  // make sure alarm_handler isn't already
+                                   // running
           p->alarm_ticks = p->alarm_interval;
           // jump to execute alarm_handler
-          *p->alarm_trapframe = *p->trapframe; // backup trapframe
+          *p->alarm_trapframe = *p->trapframe;  // backup trapframe
           p->trapframe->epc = (uint64)p->alarm_handler;
           p->alarm_goingoff = 1;
         }
@@ -98,9 +97,14 @@ usertrap(void)
 //
 // return to user space
 //
-void
-usertrapret(void)
-{
+void usertrapret(void) {
+  // 1.
+  // 进程中断栈帧保存进程内核页表、进程内核栈、陷阱管理器的选择陷阱类型的函数指针
+  // 2. 进程中断栈帧恢复用户态下一条指令到下一条指令寄存器
+  // 3. 进程中断指针恢复进程用户页表到当前页表寄存器。
+  // 4. 保存所有寄存器到进程中断上下文
+  // 5. 恢复进程中断上下文的
+
   struct proc *p = myproc();
 
   // we're about to switch the destination of traps from
@@ -113,18 +117,18 @@ usertrapret(void)
 
   // set up trapframe values that uservec will need when
   // the process next re-enters the kernel.
-  p->trapframe->kernel_satp = r_satp();         // kernel page table
-  p->trapframe->kernel_sp = p->kstack + PGSIZE; // process's kernel stack
+  p->trapframe->kernel_satp = r_satp();          // kernel page table
+  p->trapframe->kernel_sp = p->kstack + PGSIZE;  // process's kernel stack
   p->trapframe->kernel_trap = (uint64)usertrap;
-  p->trapframe->kernel_hartid = r_tp();         // hartid for cpuid()
+  p->trapframe->kernel_hartid = r_tp();  // hartid for cpuid()
 
   // set up the registers that trampoline.S's sret will use
   // to get to user space.
-  
+
   // set S Previous Privilege mode to User.
   unsigned long x = r_sstatus();
-  x &= ~SSTATUS_SPP; // clear SPP to 0 for user mode
-  x |= SSTATUS_SPIE; // enable interrupts in user mode
+  x &= ~SSTATUS_SPP;  // clear SPP to 0 for user mode
+  x |= SSTATUS_SPIE;  // enable interrupts in user mode
   w_sstatus(x);
 
   // set S Exception Program Counter to the saved user pc.
@@ -133,37 +137,33 @@ usertrapret(void)
   // tell trampoline.S the user page table to switch to.
   uint64 satp = MAKE_SATP(p->pagetable);
 
-  // jump to trampoline.S at the top of memory, which 
+  // jump to trampoline.S at the top of memory, which
   // switches to the user page table, restores user registers,
   // and switches to user mode with sret.
   uint64 fn = TRAMPOLINE + (userret - trampoline);
-  ((void (*)(uint64,uint64))fn)(TRAPFRAME, satp);
+  ((void (*)(uint64, uint64))fn)(TRAPFRAME, satp);
 }
 
 // interrupts and exceptions from kernel code go here via kernelvec,
 // on whatever the current kernel stack is.
-void 
-kerneltrap()
-{
+void kerneltrap() {
   int which_dev = 0;
   uint64 sepc = r_sepc();
   uint64 sstatus = r_sstatus();
   uint64 scause = r_scause();
-  
-  if((sstatus & SSTATUS_SPP) == 0)
-    panic("kerneltrap: not from supervisor mode");
-  if(intr_get() != 0)
-    panic("kerneltrap: interrupts enabled");
 
-  if((which_dev = devintr()) == 0){
+  if ((sstatus & SSTATUS_SPP) == 0)
+    panic("kerneltrap: not from supervisor mode");
+  if (intr_get() != 0) panic("kerneltrap: interrupts enabled");
+
+  if ((which_dev = devintr()) == 0) {
     printf("scause %p\n", scause);
     printf("sepc=%p stval=%p\n", r_sepc(), r_stval());
     panic("kerneltrap");
   }
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING)
-    yield();
+  if (which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING) yield();
 
   // the yield() may have caused some traps to occur,
   // so restore trap registers for use by kernelvec.S's sepc instruction.
@@ -171,9 +171,7 @@ kerneltrap()
   w_sstatus(sstatus);
 }
 
-void
-clockintr()
-{
+void clockintr() {
   acquire(&tickslock);
   ticks++;
   wakeup(&ticks);
@@ -185,41 +183,37 @@ clockintr()
 // returns 2 if timer interrupt,
 // 1 if other device,
 // 0 if not recognized.
-int
-devintr()
-{
+int devintr() {
   uint64 scause = r_scause();
 
-  if((scause & 0x8000000000000000L) &&
-     (scause & 0xff) == 9){
+  if ((scause & 0x8000000000000000L) && (scause & 0xff) == 9) {
     // this is a supervisor external interrupt, via PLIC.
 
     // irq indicates which device interrupted.
     int irq = plic_claim();
 
-    if(irq == UART0_IRQ){
+    if (irq == UART0_IRQ) {
       uartintr();
-    } else if(irq == VIRTIO0_IRQ){
+    } else if (irq == VIRTIO0_IRQ) {
       virtio_disk_intr();
-    } else if(irq){
+    } else if (irq) {
       printf("unexpected interrupt irq=%d\n", irq);
     }
 
     // the PLIC allows each device to raise at most one
     // interrupt at a time; tell the PLIC the device is
     // now allowed to interrupt again.
-    if(irq)
-      plic_complete(irq);
+    if (irq) plic_complete(irq);
 
     return 1;
-  } else if(scause == 0x8000000000000001L){
+  } else if (scause == 0x8000000000000001L) {
     // software interrupt from a machine-mode timer interrupt,
     // forwarded by timervec in kernelvec.S.
 
-    if(cpuid() == 0){
+    if (cpuid() == 0) {
       clockintr();
     }
-    
+
     // acknowledge the software interrupt by clearing
     // the SSIP bit in sip.
     w_sip(r_sip() & ~2);
@@ -230,7 +224,9 @@ devintr()
   }
 }
 
-int sigalarm(int ticks, void(*handler)()) {
+int sigalarm(int ticks, void (*handler)()) {
+  // 1. 设置进程的时钟处理函数，剩余时间
+
   struct proc *p = myproc();
   p->alarm_interval = ticks;
   p->alarm_handler = handler;
@@ -240,6 +236,9 @@ int sigalarm(int ticks, void(*handler)()) {
 
 int sigreturn() {
   // resume the state of trapframe to how it is before the alarm goes off.
+  // 1. 将进程的中断上下文恢复为进程的alarm中断之前的进程中断上下文
+  // 2. 标记进程alarm中断已结束
+
   struct proc *p = myproc();
   *p->trapframe = *p->alarm_trapframe;
   p->alarm_goingoff = 0;
